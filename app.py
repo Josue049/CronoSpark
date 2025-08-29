9dcdb835ca1f54086f81da227c99119fa1effe48
Josue049 <josue09guti@gmail.com>
2025-08-29 02:06:32 -0500

    correción con modelos de db

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from functools import wraps
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from models import db
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
